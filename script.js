080ad8b87666fd3c4486aa48cdddb659a2e9bca0
nathandev254 <warimwenathaniel@gmail.com>
2023-04-26 06:30:20 +0300

    Morning buddy

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const todo = document.querySelector(".Todo-input");
 const button = document.querySelector(".btn");
-const TodoContainer = document.querySelector('.Todo-container')
+const TodoContainer = document.querySelector(".Todo-container");
+
+// Initializing a new object dynamically to store the todos 
+const todos = new Array
+
+
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
-  const html = `  <div class="output">
-  <p>${todo.value}</p>
-  <div class="icon-container">
-    <i class="fa-solid fa-trash icon"></i>
-  </div>
-</div>`;
- 
-    TodoContainer.insertAdjacentHTML('beforeend', html)
+  if (todo.value) {
+    todos.push(todo.value)
+   
+    //Stores the todo data in localStorage
+    localStorage.setItem('todos',JSON.stringify(todos))
+    
+    const html = `  <div class="output">
+    <p>${todo.value}</p>
+    <div class="icon-container">
+      <i class="fa-solid fa-trash icon"></i>
+    </div>
+  </div>`;
 
-    todo.value = ''
-  // console.log(todo.value)
-});
+    TodoContainer.insertAdjacentHTML("beforeend", html);
+
+    todo.value = "";
+  } 
+  else return
+})
+
+
+
+
+
