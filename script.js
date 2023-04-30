3c75f0b7fe159b3b7c2f859b769de042deb8ff09
nathandev254 <warimwenathaniel@gmail.com>
2023-04-30 11:54:42 +0300

    Code refactoring

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 const todo = document.querySelector(".Todo-input");
 const button = document.querySelector(".btn");
 const TodoContainer = document.querySelector(".Todo-container");
+const icon = document.querySelectorAll('.icon')
 
 // Initializing a new object dynamically to store the todos 
 const todos = new Array
 
+//function off adding Todoitems
+const addTodos = function(todo,todos){
+  if (todo) {
+    todos.push(todo)
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+}
 
-
-button.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (todo.value) {
-    todos.push(todo.value)
-   
-    //Stores the todo data in localStorage
-    localStorage.setItem('todos',JSON.stringify(todos))
-    
+// Function of displaying todo items
+const Displaytodos = function(todos){
+  todos.forEach(element => {
     const html = `  <div class="output">
-    <p>${todo.value}</p>
+    <p>${element}</p>
     <div class="icon-container">
       <i class="fa-solid fa-trash icon"></i>
     </div>
   </div>`;
 
     TodoContainer.insertAdjacentHTML("beforeend", html);
+  });
+}
 
+
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+   addTodos(todo.value,todos)
+   
+  Displaytodos(todos);
+
+   
+
+  // console.log(localStorage)
     todo.value = "";
-  } 
-  else return
-})
-
-
-
+  })
 
 
